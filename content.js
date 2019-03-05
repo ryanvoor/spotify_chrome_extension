dcee0cf3d509f5cbb43a3b2cf50a8583ce0e9f46
Ryan Voor <rvoor@neadwerx.com>
2019-03-05 15:52:55 -0500

    make the margin-bottom area under the now-playing-bar look better

--- content.js
+++ content.js
@@ -3,7 +3,7 @@ if ( document.getElementsByTagName( 'body' )[0].clientHeight > 1200 ) {
     // when the playing bar is on the page
     if ( typeof document.getElementsByClassName( 'now-playing-bar-container' )[0] !== 'undefined' ) {
         console.log( 'Putting extra space under the footer (place where the "now playing" bar is) for ease of viewing on vertical monitors' );
-        document.getElementsByClassName( 'now-playing-bar-container' )[0].style.marginBottom = '500px';
+        document.getElementsByClassName( 'now-playing-bar' )[0].style.marginBottom = '500px';
         document.getElementsByClassName( 'main-view-container__content' )[0].style.marginBottom = '500px';
     }
 }
