a56ab233601cb9bb0f7adcd5057895959feb1f7f
Ryan Voor <rvoor@neadwerx.com>
2021-04-01 13:44:37 -0400

    update to account for new spotify html layout

--- content.js
+++ content.js
@@ -3,9 +3,9 @@ if ( document.getElementsByTagName( 'body' )[0].clientHeight > 1200 ) {
     var checkPageLoadedYet = setInterval(
         function() {
             // when the playing bar is on the page
-            if ( typeof document.getElementsByClassName( 'now-playing-bar-container' )[0] !== 'undefined' ) {
+            if ( typeof document.getElementsByClassName( 'Root__now-playing-bar' )[0] !== 'undefined' ) {
                 console.log( 'Putting extra space under the footer (place where the "now playing" bar is) for ease of viewing on vertical monitors' );
-                document.getElementsByClassName( 'now-playing-bar' )[0].style.marginBottom = '500px';
+                document.getElementsByClassName( 'Root__now-playing-bar' )[0].children[0].style.marginBottom = '500px';
 
                 clearInterval( checkPageLoadedYet );
             }
